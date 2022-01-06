f86f2707d64a911d18f28ac9f50650d81151ad47
camille-granier <granierc@protonmail.com>
2022-01-06 13:03:25 +0100

    filter buttons

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,12 +10,21 @@ const Products = () => {
     const [playOnce, setPlayOnce] = useState(true);
 
     useEffect(() => {
+
+        setLoading(true);
+
         axios.get("https://fakestoreapi.com/products")
             .then((res) => {setData(res.data);
                            setFilter(res.data);
-                           setPlayOnce(false)});
+                           setPlayOnce(false);
+                           setLoading(false)});
 
     }, []);
+
+    const filteredProducts = (cat) => {
+        const updatedList = data.filter((x) => x.category === cat);
+        setFilter(updatedList);
+    }
 
     const Loading = () => {
         return (
@@ -26,14 +35,21 @@ const Products = () => {
     const ShowProducts = () => {
         return (
             <>
+                <div className='buttons'>
+                    <button className='btn btn-outline-dark' onClick={() => setFilter(data)}>All</button>
+                    <button className='btn btn-outline-dark' onClick={() => filteredProducts("Men's Clothing")}>Men's Clothing</button>
+                    <button className='btn btn-outline-dark' onClick={() => filteredProducts("Women's clothing")}>Women's Clothing</button>
+                    <button className='btn btn-outline-dark' onClick={() => filteredProducts("Jewelery")}>Jewellery</button>
+                    <button className='btn btn-outline-dark' onClick={() => filteredProducts("electronics")}>Electronics</button>
+                </div>
                 {filter.map((product) => ( 
                    <>
                         <div className='col-3'>
                             <div className='card'>
                                 <img className='card-img-top' src={product.image} alt={product.title}/>
                                 <div className='card-body'>
-                                    <h5></h5>
-                                    <p></p>
+                                    <h5>{product.title.substring(0, 12)}</h5>
+                                    <p>€{product.price}</p>
                                 </div>
                             </div>
                         </div>
